bf1634972c6e6a6e7abfcfbec5db6df457befe3a
U-ZBOOK5\sl <stuart.lynne@gmail.com>
2025-04-10 11:48:03 -0700

    update

--- updatelap.sh
+++ updatelap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
 
-scp sl@whiskey:~/work25/racesupport/lapcounter/Lap*html "/cygdrive/c/Program Files/CrossMgr/CrossMgrHtml/"
+cp Lap*html "/cygdrive/c/Program Files/CrossMgr/CrossMgrHtml/"
 
-scp sl@whiskey:~/work25/racesupport/lapcounter/Lap*html .
